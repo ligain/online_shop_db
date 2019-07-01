a121b5378f7980447896feda62e7f60ac6fe7026
ligain <khokhlov86@gmail.com>
2019-07-01 23:59:23 +0300

    Added price table to init_db.sql Changed data types in schema

--- init_db.sql
+++ init_db.sql
@@ -14,6 +14,13 @@ CREATE TABLE IF NOT EXISTS public."attribute" (
     PRIMARY KEY (id)
 );
 
+-- DROP TABLE public.attribute_list_value;
+CREATE TABLE IF NOT EXISTS public.attribute_list_value (
+    attribute_id int4 NOT NULL,
+    value varchar(45),
+	CONSTRAINT fk_attribute_list_value_attribute1 FOREIGN KEY (attribute_id) REFERENCES attribute(id) DEFERRABLE INITIALLY DEFERRED
+);
+
 -- DROP TABLE public.product_category;
 CREATE TABLE IF NOT EXISTS public.product_category (
     id serial NOT NULL,
@@ -32,7 +39,6 @@ CREATE TABLE IF NOT EXISTS public.product_category_attribute (
     CONSTRAINT fk_product_category_has_attribute_attribute1 FOREIGN KEY (attribute_id) REFERENCES "attribute"(id) DEFERRABLE INITIALLY DEFERRED
 );
 
-
 -- DROP TABLE public.product;
 CREATE TABLE IF NOT EXISTS public.product (
     id serial NOT NULL,
@@ -41,7 +47,33 @@ CREATE TABLE IF NOT EXISTS public.product (
     manufacturer_id int4 NOT NULL,
     product_category_id int4 NOT NULL,
     PRIMARY KEY (id),
+    CHECK (amount >= 0),
     CONSTRAINT fk_product_manufacturer1 FOREIGN KEY (manufacturer_id) REFERENCES manufacturer(id) DEFERRABLE INITIALLY DEFERRED,
     CONSTRAINT fk_product_product_category1 FOREIGN KEY (product_category_id) REFERENCES product_category(id) DEFERRABLE INITIALLY DEFERRED
 );
+
+-- DROP TABLE public.product_attribute;
+CREATE TABLE IF NOT EXISTS public.product_attribute (
+    product_id int4 NOT NULL,
+    attribute_id int4 NOT NULL,
+    value varchar(45),
+    UNIQUE (product_id, attribute_id),
+    CONSTRAINT fk_product_has_attribute_product1 FOREIGN KEY (product_id) REFERENCES product(id) DEFERRABLE INITIALLY DEFERRED,
+    CONSTRAINT fk_product_has_attribute_attribute1 FOREIGN KEY (attribute_id) REFERENCES "attribute"(id) DEFERRABLE INITIALLY DEFERRED
+);
+
+-- DROP TABLE public.price
+CREATE TABLE IF NOT EXISTS public.price (
+	id serial NOT NULL,
+	product_id int4 NOT NULL,
+	effective_start_date timestamp DEFAULT now(),
+	effective_end_date timestamp,
+	value int4 NOT NULL DEFAULT 0,
+	rebate int4 NOT NULL DEFAULT 0,
+	CHECK (value >= 0),
+	CHECK (rebate >= 0),
+	PRIMARY KEY(id),
+	CONSTRAINT fk_price_product1 FOREIGN KEY (product_id) REFERENCES product(id) DEFERRABLE INITIALLY DEFERRED
+);
+
 
